0ad7b996cd75fcb06880f9ff134a44e73538fd12
kapphire <nodejs1987@gmail.com>
2024-11-21 05:05:29 -0800

    added unique field to address

--- addresses/views.py
+++ addresses/views.py
@@ -1,5 +1,6 @@
 from addresses.models import Address
 from addresses.serializers import AddressSerializer
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import status
 from addresses.handlers import SolanaTokenAnalyzer
@@ -12,22 +13,20 @@ class AddressList(generics.ListCreateAPIView):
     serializer_class = AddressSerializer
 
     def create(self, request, *args, **kwargs):
-        # Custom logic before saving
         data = request.data
-        token_analyzer = SolanaTokenAnalyzer(data.get('address'))
+        address = data.get('address')
+
+        # Proceed with your custom logic
+        token_analyzer = SolanaTokenAnalyzer(address)
         token_data = token_analyzer.update_holder_data()
 
-        # Use the serializer to validate the data
+        # Validate and save the object
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
-
-        # Save the object
         if token_data:
-            self.perform_create(serializer)
+            if not Address.objects.filter(address=address).exists():
+                self.perform_create(serializer)
 
-        # Custom logic after saving
-        # response_data = serializer.data
+        # Return custom response
         response_data = token_data
-
-        # Return a custom response
         return Response(response_data, status=status.HTTP_201_CREATED)
