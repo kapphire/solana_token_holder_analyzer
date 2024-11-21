2ccc19278f7779677984043f1285895b07ffcf98
kapphire <nodejs1987@gmail.com>
2024-11-21 04:54:14 -0800

    address field unique

--- addresses/models.py
+++ addresses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Address(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, unique=True)
 
     class Meta:
         ordering = ['created']
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-acjdq(llv+s0r-hla^33xasl1bknxi0r&-*5fh(!6h6geth_f1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['54.160.184.59', 'localhost']
 
