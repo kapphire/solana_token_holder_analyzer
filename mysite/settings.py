e7c8ae86ba54ac0567b9906122dba1667e947ec7
kapphire <nodejs1987@gmail.com>
2024-11-21 05:26:06 -0800

    cors config updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['54.160.184.59', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'corsheaders',
     'rest_framework',
     'addresses',
 ]
@@ -120,14 +120,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://54.160.184.59/",
 ]
-
-CORS_ALLOW_CREDENTIALS = True
 
 # Optional: Allow specific methods (GET, POST, etc.)
 CORS_ALLOW_METHODS = [
@@ -139,15 +138,3 @@ CORS_ALLOW_METHODS = [
     'OPTIONS',
 ]
 
-# Optional: Allow specific headers like Authorization, etc.
-CORS_ALLOW_HEADERS = [
-    'content-type',
-    'authorization',
-    'x-csrftoken',
-    'accept',
-    'accept-encoding',
-    'accept-language',
-    'origin',
-    'user-agent',
-    'x-requested-with',
-]
